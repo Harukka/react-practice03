bb5e083e43d31e4a27424f5ae623ea0d0f794c51
Harukka <goooonza@gmail.com>
2024-07-10 12:54:53 +0900

    added images

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 const pageFlip = new St.PageFlip(document.getElementById("book"), {
     width: 400,
-    height: 400,
+    height: 533.33,
     showCover: true,
+    usePortrait: true,
   });
   
   pageFlip.loadFromHTML(document.querySelectorAll(".my-page"));　
