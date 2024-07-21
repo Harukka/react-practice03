4efd0050a3445e050cd521bfebe12e64a1c77389
Harukka <goooonza@gmail.com>
2024-07-21 14:24:24 +0900

    updated

--- js/main.js
+++ js/main.js
@@ -3,6 +3,16 @@ const pageFlip = new St.PageFlip(document.getElementById("book"), {
     height: 533.33,
     showCover: true,
     usePortrait: true,
+    // size: "stretch",
+    // minWidth: 315,
+    // maxWidth: 1000,
+    // minHeight: 420,
+    // maxHeight: 1350,
+    // drawShadow: true,
+    flippingTime: 2000,
+    // usePortrait: true,
+    startZIndex: 1,
+    autoSize: true
   });
   
-  pageFlip.loadFromHTML(document.querySelectorAll(".my-page"));　
+  pageFlip.loadFromHTML(document.querySelectorAll(".my-page"));
